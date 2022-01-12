f3ed60f39496a166befdd2d75e35fbdd7990d01b
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-12 12:41:58 -0300

    Next Topic: Nesting elements

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,4 @@
-git init
 git add -A
-git commit -m "Studies started"
-git branch -M master
-git remote add origin https://github.com/CarlosViniMSouza/Studies-MDN.git
-git push -u origin master
+git commit -m "Next Topic: Nesting elements"
+git push
 git status
